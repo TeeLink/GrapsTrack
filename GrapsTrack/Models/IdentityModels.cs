f412c9f132aa2a386ef70a61603143e3b67efc56
Tee Link <tesuanseylink@gmail.com>
2016-05-04 11:02:05 -0500

    Models Set tup

--- GrapsTrack/Models/IdentityModels.cs
+++ GrapsTrack/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -16,6 +17,11 @@ namespace GrapsTrack.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
